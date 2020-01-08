eb811d5ee64aeae7f55f5e42d3dd4e616e80cf99
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-01-08 02:36:42 -0500

    getUser created

--- ts/data/models/users.ts
+++ ts/data/models/users.ts
@@ -1,46 +1,16 @@
-import db from '../dbConfig';
-// import { User } from '../../types';
-// import { basicModelTemplate } from './basicModelTemplate';
+import { User } from '../../types';
+import { basicModelTemplate } from './basicModelTemplate';
+import { convertObjectCamelToSnake, convertObjectSnakeToCamel } from '../../utils';
 
-(async () => {
-  const result = await db('amenities');
-  console.log(result);
-})();
+const model = basicModelTemplate<User>({
+  tableName: 'users',
+  preprocessData: ((data) => convertObjectCamelToSnake({ obj: data })),
+  processResult: ((result) => convertObjectSnakeToCamel({ obj: result })),
+});
 
-export default {};
-
-// const model = basicModelTemplate<User>({
-//   tableName: 'users',
-//   preprocessData: ({ username, password }) => ({
-//     username,
-//     password: hashedPassword,
-//   }),
-//   processResult: ({ id, username }) => ({
-//     id,
-//     username,
-//   }),
-// });
-
-// interface GetByUsernameArg {
-//   username: string,
-// }
-
-// const getByUsername = ({ username }: GetByUsernameArg) => (db('user_credentials')
-//   .where({ username })
-//   .first()
-//   .then((result) => (
-//     (result)
-//       ? {
-//         id: result.id,
-//         username: result.username,
-//         hashedPassword: result.hashed_password,
-//       }
-//       : undefined
-//   ))
-// );
-
-// export default {
-//   get: model.get,
-//   insert: model.insert,
-//   getByUsername,
-// };
+export default {
+  get: model.get,
+  insert: model.insert,
+  update: model.update,
+  remove: model.remove,
+};
--- ts/routes/users.ts
+++ ts/routes/users.ts
@@ -1,65 +1,47 @@
 import * as Express from 'express';
 
 // import { UserCredentials as UserCredentialsType } from '../types';
-import { UserCredentials as Users } from '../data/models';
+import { Users } from '../data/models';
 
 
 export const router = Express.Router();
 
-const getUsers = async (req: Express.Request, res: Express.Response) => {
-  // const { id } = req.params;
-
-  // make sure id is a positive int
-  // if (username === undefined || password === undefined) {
-  //   return (res.status(400).json({ message: 'must provide username and password' }));
-  // }
+const getUser = async (req: Express.Request, res: Express.Response) => {
+  const { id } = req.params;
 
   try {
-    const [result] = await Users.get();
+    const [result] = await Users.get({ id: parseInt(id, 10) });
     if (result) {
-      // const token = generateToken(result);
-      // return res.status(201).json({ token });
       return res.status(200).json(result);
     }
-    return res.status(500).json({ message: 'error getting users' });
+    return res.status(500).json({ message: `error getting user by id ${id}` });
   } catch (err) {
     return res.status(500).json({
       error: err.message,
-      message: 'error getting users',
+      message: `error getting user by id ${id}`,
     });
   }
 };
 
-console.log('line 33');
-try {
-  const [result]
-}
+const getUserListings = async (req: Express.Request, res: Express.Response) => {
+  const { id } = req.params;
 
+  try {
+    const [result] = await Users.get({ id: parseInt(id, 10) });
+    if (result) {
+      return res.status(200).json(result);
+    }
+    return res.status(500).json({ message: `error getting user by id ${id}` });
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+      message: `error getting user by id ${id}`,
+    });
+  }
+};
 
-// const getUserListings = async (req: Express.Request, res: Express.Response) => {
-//   const { username, password } = req.body;
-
-//   if (username === undefined || password === undefined) {
-//     return (res.status(400).json({ message: 'must provide username and password' }));
-//   }
-
-//   try {
-//     const result = await UserCredentials.getByUsername({ username }) as UserCredentialsType;
-//     if (!!result && Bcrypt.compareSync(password, result.hashedPassword)) {
-//       const token = generateToken(result);
-//       return res.status(200).json({ token });
-//     }
-//     return res.status(403).json({ message: 'invalid credentials' });
-//   } catch (error) {
-//     return res.status(500).json({
-//       error: 'error logging in',
-//       message: error.message,
-//     });
-//   }
-// };
-
-router.get('/', getUsers);
-// router.get('/:id', getUser);
+router.get('/:id', getUser);
 // router.get('/:id/listings', getUserListings);
+// router.put('/:id/listings', updateUserListings);
 
 export default {};
